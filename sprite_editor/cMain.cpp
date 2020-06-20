b7ffb598c8a3e896f436fcd8c0db1524540a412b
o-khytrov <o.khytrov@gmail.com>
2020-06-20 15:16:58 +0300

    Add menu

--- sprite_editor/cMain.cpp
+++ sprite_editor/cMain.cpp
@@ -1,8 +1,21 @@
 #include "cMain.h"
 
-cMain::cMain() : wxFrame(nullptr,wxID_ANY,"Sprite editor")
-{
+wxBEGIN_EVENT_TABLE(cMain, wxMDIParentFrame)
 
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxMDIParentFrame(nullptr,wxID_ANY,"Sprite editor")
+{
+	m_MenuBar = new wxMenuBar();
+	this->SetMenuBar(m_MenuBar);
+	wxMenu *menuFile = new wxMenu();
+
+	menuFile->Append(10001, "New");
+	menuFile->Append(10002, "Open");
+	menuFile->Append(10003, "Save");
+	menuFile->Append(10004, "Exit");
+
+	m_MenuBar->Append(menuFile,"File");
 }
 
 
--- sprite_editor/cMain.h
+++ sprite_editor/cMain.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "wx/wx.h"
 
-class cMain :public wxFrame
+class cMain :public wxMDIParentFrame
 {
 public:
 	cMain();
 	~cMain();
-
+private:
+	wxToolBar *m_ToolBar = nullptr;
+	wxMenuBar* m_MenuBar = nullptr;
+	wxDECLARE_EVENT_TABLE();
 };
 
